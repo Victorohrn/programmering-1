ae9f60506449f6fe0d280b62724a25e06ad369d4
Victorohrn <42860932+Victorohrn@users.noreply.github.com>
2018-09-12 15:11:54 +0200

    Programmering 12/9

--- src/printing/Numbers.java
+++ src/printing/Numbers.java
@@ -1,10 +1,16 @@
 package printing;
 
 public class Numbers {
-
+	public static final double height = 8.5;
+	public static final double width = 5.5;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println(Radius = 7.5);
+		double perimeter = 2 *(height + width);
+		double area = height * width;
+		
+		
+System.out.println("Perimeter is = " + perimeter);
+System.out.println("Area is = " + area);
 
 	}
 	
