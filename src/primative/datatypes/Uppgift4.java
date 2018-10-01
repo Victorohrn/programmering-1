7ddbdba3fde7c1a85e7934e64ef7f566cc94bb26
Victorohrn <42860932+Victorohrn@users.noreply.github.com>
2018-10-01 11:48:41 +0200

    Uppgift4

---
+++ src/primative/datatypes/Uppgift4.java
@@ -0,0 +1,16 @@
+package primative.datatypes;
+
+public class Uppgift4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		float f = 0.5671f;
+		f =  Math.round(f * 100) / 100f;
+		System.out.println("Result: " + f);
+		
+		
+		
+	}
+
+}
