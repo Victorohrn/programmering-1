36c369d484812ccf9b4352c6c595df5d614f5b75
Victorohrn <42860932+Victorohrn@users.noreply.github.com>
2018-10-01 11:31:57 +0200

    Uppgift2

---
+++ src/primative/datatypes/Uppgift2.java
@@ -0,0 +1,15 @@
+package primative.datatypes;
+
+public class Uppgift2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		double res = Math.nextDown(2.7/3.9);
+		System.out.println("Result: " + res);
+		
+		
+		
+	}
+
+}
