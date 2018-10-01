d325ce29634b69547b0c394a539b6ef9f3c3ed0a
Victorohrn <42860932+Victorohrn@users.noreply.github.com>
2018-10-01 11:25:31 +0200

    Uppgift1

--- src/primative/datatypes/Uppgift1.java
+++ src/primative/datatypes/Uppgift1.java
@@ -5,6 +5,10 @@ public class Uppgift1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int res = Math.floorDiv(20, 9);
+		System.out.println("Result: " + res);
+		
+		
 	}
 
 }
