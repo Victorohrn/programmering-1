f092ae73ccf1911228646443941f949425b4c8f7
Victorohrn <42860932+Victorohrn@users.noreply.github.com>
2018-10-01 11:38:02 +0200

    Uppgift 3

---
+++ src/primative/datatypes/Uppgift3.java
@@ -0,0 +1,17 @@
+package primative.datatypes;
+
+public class Uppgift3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double res = 2.0;
+		int i = 2;
+		System.out.println(res == i);
+		
+		
+		
+		
+	}
+
+}
