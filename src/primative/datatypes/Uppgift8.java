383f8a9ed17e723a00e05059fe5e6f7c09042e2d
Victorohrn <42860932+Victorohrn@users.noreply.github.com>
2018-10-03 14:57:59 +0200

    Uppgift8

---
+++ src/primative/datatypes/Uppgift8.java
@@ -0,0 +1,17 @@
+package primative.datatypes;
+
+public class Uppgift8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		float res = Math.abs(-0.45f);
+				System.out.println("Result: " + res);
+		
+		
+		
+		
+		
+	}
+
+}
