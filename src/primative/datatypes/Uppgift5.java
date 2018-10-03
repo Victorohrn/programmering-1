0b8aee6e5063eaa920c6d28334d84ef9be2d7d06
Victorohrn <42860932+Victorohrn@users.noreply.github.com>
2018-10-03 14:16:40 +0200

    uppgift 5

---
+++ src/primative/datatypes/Uppgift5.java
@@ -0,0 +1,17 @@
+package primative.datatypes;
+
+public class Uppgift5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	
+		int res = Integer.reverse(2);
+		System.out.println("Result: " + res);
+		
+		
+		
+		
+	}
+
+}
