e9c59d3290b0b93f3efc59b2aa6844d613f3f15a
Victorohrn <42860932+Victorohrn@users.noreply.github.com>
2018-10-03 14:55:18 +0200

    Uppgift 7

---
+++ src/primative/datatypes/Uppgift7.java
@@ -0,0 +1,18 @@
+package primative.datatypes;
+
+public class Uppgift7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int i = Math.abs(-7);
+		System.out.println("Result: " + i);
+		
+		
+		
+		
+		
+		
+	}
+
+}
