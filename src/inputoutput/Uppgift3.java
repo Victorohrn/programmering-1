316b0c8933bd50991602562b2b60a1b69eb45301
Victorohrn <42860932+Victorohrn@users.noreply.github.com>
2018-10-12 09:48:19 +0200

    Uppgift3

---
+++ src/inputoutput/Uppgift3.java
@@ -0,0 +1,20 @@
+package inputoutput;
+
+import java.util.Scanner;
+
+public class Uppgift3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scanner = new Scanner(System.in);
+		while (true){System.out.print("Enter a number: ");
+		int number1 = scanner.nextInt();
+		for(int i = 1; i <= 10; i++) {
+			System.out.println("" + number1 + " x " + i + " = " + number1 * i);
+			}
+			break;
+		}
+	}
+
+}
---
+++ src/inputoutput/Uppgift4.java
@@ -0,0 +1,10 @@
+package inputoutput;
+
+public class Uppgift4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
