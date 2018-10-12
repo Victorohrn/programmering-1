c21319b9e7d3981a7fcff3cf26c0f68b79a7a81a
Victorohrn <42860932+Victorohrn@users.noreply.github.com>
2018-10-12 14:46:31 +0200

    UpdatedUppgift2

--- src/inputoutput/Uppgift2.java
+++ src/inputoutput/Uppgift2.java
@@ -10,10 +10,10 @@ public class Uppgift2 {
 		Scanner scanner = new Scanner(System.in);
 		while (true) {
 			System.out.print("Enter a firstnumber: ");
-			int number1 = scanner.nextInt();
+			double number1 = scanner.nextDouble();
 			System.out.print("Enter a secondnumber: ");
-			int number2 = scanner.nextInt();
-			System.out.println("" + number1 + " + " + number2 + " = " + number1 + number2);
+			double number2 = scanner.nextDouble();
+			System.out.println("" + number1 + " + " + number2 + " = " + (number1 + number2));
 			System.out.println("" + number1 + " - " + number2 + " = " + (number1 - number2));
 			System.out.println("" + number1 + " x " + number2 + " = " + number1 * number2);
 			System.out.println("" + number1 + " / " + number2 + " = " + number1 / number2);
---
+++ src/inputoutput/Uppgift6.java
@@ -0,0 +1,27 @@
+package inputoutput;
+
+import java.util.Scanner;
+
+public class Uppgift6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scanner = new Scanner(System.in);
+		while (true) {
+			System.out.println("Enter the height of the rektangle: ");
+			double h = scanner.nextDouble();
+			System.out.println("Enter the width of the rektangle: ");
+			double w = scanner.nextDouble();
+			System.out.println("Area is: " + h * w);
+			System.out.println("Perimeter is: " " " + (h + w) + "* 2 " 2 * (h + w));
+		}
+	
+		
+		
+		
+		
+		
+	}
+
+}
