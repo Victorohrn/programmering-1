c5cfb4c20f9928321cf662599568e932183bb5c4
Victorohrn <42860932+Victorohrn@users.noreply.github.com>
2018-10-12 09:16:32 +0200

    uppgift1input

---
+++ src/jonaslektionsuppgifter/JonasUppgift1.java
@@ -0,0 +1,15 @@
+package jonaslektionsuppgifter;
+
+public class JonasUppgift1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		
+	       for(int i = 0;i >= 0;i++){
+	            System.out.println(Integer.toOctalString(i,8)); //optionally use "Integer.toString(i, 8)"
+	        }
+	    }
+	}
