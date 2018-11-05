b67b9170a73c24ff948db92fca05c4325e05f6d2
Victorohrn <42860932+Victorohrn@users.noreply.github.com>
2018-11-05 11:49:32 +0100

    Update uppgift7

--- src/primative/datatypes/Uppgift7.java
+++ src/primative/datatypes/Uppgift7.java
@@ -1,13 +1,18 @@
 package primative.datatypes;
 
+import java.util.Scanner;
+
 public class Uppgift7 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		int i = Math.abs(-7);
-		System.out.println("Result: " + i);
-
+		while (true) {
+		Scanner scanner = new Scanner (System.in);
+		System.out.println("Enter a integer that you want absolute value from: ");
+		int j = scanner.nextInt();
+		int i = Math.abs(j);
+		System.out.println("The absolute value from " + j + " is: "  +  i);
+		}
 	}
 
 }
